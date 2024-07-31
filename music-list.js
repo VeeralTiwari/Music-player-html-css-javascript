a4814bd180c43cdaa22d1aaaeac03c36778ebf6e
Veeral Tiwari <100480162+VeeralTiwari@users.noreply.github.com>
2024-07-31 21:53:09 +0530

    Create music-list.js

---
+++ music-list.js
@@ -0,0 +1,40 @@
+
+
+const songs = [
+    {
+        title: "Mitti di Khushboo",
+        artist: "Ayushmaan Khurana",
+        path: "MP3 files/Mitti-Di-Khushboo_320(PaglaSongs).mp3",
+        image: "images/Mitti-Di-Khushboo-Punjabi-2014-500x500.jpg",
+        width: "280px",
+        liked : false,
+        length: "04:07"
+    },
+    {
+        title: "Ranjhanaa",
+        artist: "A.R. Rahman, Jaswinder Singh and Shiraz Uppal",
+        path: "MP3 files/Raanjhanaa-Hua-Mai-Tera_320(PaglaSongs).mp3",
+        image: "images/Ranjhana.jpg",
+        width: "280px",
+        liked : false,
+        length : "03:10"
+    },
+    {
+        title: "Tum hi ho bandhu",
+        artist: "Pritam",
+        path: "MP3 files/Tumhi Ho Bandhu Sakha Tumhi_320(PagalWorld.com.so).mp3",
+        image: "images/artworks-000026255025-ocuoez-t500x500.jpg",
+        width: "280px",
+        liked : false,
+        length : "04:24"
+    },
+    {
+        title: "Pehli Nazar Me",
+        artist: "Atif Aslam",
+        path: "MP3 files/Pehli-Nazar-Mein-Kaise-Jaado-Kar-Diya_320(PaglaSongs).mp3",
+        image: "images/Pehli-nazar-mein.jpeg",
+        width: "280px",
+        liked : false,
+        length : "03:17"
+    },
+];
